07f123aeec0c4307b4879272625dce690cddb712
unknown <young_min12@naver.com>
2018-11-13 11:02:48 +0900

    hihi

---
+++ src/swap.java
@@ -0,0 +1,11 @@
+public class swap {
+
+	public static void main(String[] args) {
+	int n=10, m=20, tmp;
+	System.out.println("x:"+n+", y:"+m);
+	tmp=n;
+	n=m;
+	m=tmp;
+	System.out.println("x:"+n+", y:"+m);
+	}
+}
